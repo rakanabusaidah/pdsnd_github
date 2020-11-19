30dd50996dd42ddb12b59b0d744777169d7d6ac9
rakanabusaidah <rakanmya@gmail.com>
2020-11-19 23:31:27 +0300

    Added an informative comments in bikeshare_2 file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-
+# The available data
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
